f018547cb886b171ecf4e42e1064c3bd788bd199
EuihyeonO <yuu5666@naver.com>
2023-10-28 18:19:51 +0900

    조이스틱 문제 해결

--- CodingTest/CodingTest/체육복.cpp
+++ CodingTest/CodingTest/체육복.cpp
@@ -1,54 +1,54 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(int n, vector<int> lost, vector<int> reserve) 
-{
-	std::vector<int> Students;
-	Students.resize(n, 1);
-
-	for (int i = 0; i < lost.size(); i++)
-	{
-		Students[lost[i] - 1] = 0;
-	}
-
-	for (int i = 0; i < reserve.size(); i++)
-	{
-		Students[reserve[i] - 1]++;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		if (Students[i] == 0)
-		{
-			if (i != 0 && Students[i - 1] == 2)
-			{
-				Students[i - 1] = 1;
-				Students[i] = 1;
-			}
-			else if(i != n - 1 && Students[i + 1] == 2)
-			{
-				Students[i + 1] = 1;
-				Students[i] = 1;
-			}
-		}
-	}
-
-	int Count = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		if (Students[i] >= 1)
-		{
-			Count++;
-		}
-	}
-
-	return Count;
-}
-
-int main()
-{
-	return 0;
-}
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int n, vector<int> lost, vector<int> reserve) 
+//{
+//	std::vector<int> Students;
+//	Students.resize(n, 1);
+//
+//	for (int i = 0; i < lost.size(); i++)
+//	{
+//		Students[lost[i] - 1] = 0;
+//	}
+//
+//	for (int i = 0; i < reserve.size(); i++)
+//	{
+//		Students[reserve[i] - 1]++;
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (Students[i] == 0)
+//		{
+//			if (i != 0 && Students[i - 1] == 2)
+//			{
+//				Students[i - 1] = 1;
+//				Students[i] = 1;
+//			}
+//			else if(i != n - 1 && Students[i + 1] == 2)
+//			{
+//				Students[i + 1] = 1;
+//				Students[i] = 1;
+//			}
+//		}
+//	}
+//
+//	int Count = 0;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (Students[i] >= 1)
+//		{
+//			Count++;
+//		}
+//	}
+//
+//	return Count;
+//}
+//
+//int main()
+//{
+//	return 0;
+//}
