1a01b7a52e1b7cacd52cb1165e8410f49bcb9629
EuihyeonO <yuu5666@naver.com>
2023-12-28 16:54:13 +0900

    업데이트

--- CodingTest/CodingTest/Heap, Priority_Queue 구현.cpp
+++ CodingTest/CodingTest/Heap, Priority_Queue 구현.cpp
@@ -1,142 +1,142 @@
-#include <queue>
-#include <iostream>
-
-template <typename Data>
-class Priority_Queue
-{
-public:
-	Priority_Queue() 
-	{
-		Capacity = 4;
-		Arr = new Data[Capacity]();
-	}
-
-	~Priority_Queue() {}
-
-	void Push(const Data& _Data)
-	{
-		if (Size + 1 >= Capacity)
-		{
-			Reserve(Size * 2);
-		}
-
-		Arr[Size + 1] = _Data;
-		Heapify_Insert();
-		
-		Size++;
-	}
-
-	Data Top()
-	{
-		return Arr[1];
-	}
-
-	void Pop()
-	{
-		if (Size <= 0)
-		{
-			return;
-		}
-
-		Arr[1] = Arr[Size];
-		Size--;
-
-		Heapify_Delete();
-	}
-
-private:
-	void Heapify_Insert()
-	{
-		size_t NewDataIndex = Size + 1;
-		size_t ParentIndex;
-
-		while (NewDataIndex > 1)
-		{
-			ParentIndex = NewDataIndex / 2;
-
-			if (Arr[ParentIndex] < Arr[NewDataIndex])
-			{
-				Data Temp = Arr[NewDataIndex];
-				Data Copy = Arr[ParentIndex];
-
-				Arr[NewDataIndex] = Arr[ParentIndex];
-				Arr[ParentIndex] = Temp;
-
-				NewDataIndex = ParentIndex;
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
-
-	void Heapify_Delete()
-	{
-		size_t CurIndex = 1;
-
-		while (CurIndex <= (Size / 2))
-		{
-			size_t LeftChild = CurIndex * 2;
-			size_t RightChild = CurIndex * 2 + 1;
-
-			size_t MaxChild = Arr[LeftChild] < Arr[RightChild] ? RightChild : LeftChild;
-
-			if (Arr[MaxChild] > Arr[CurIndex])
-			{
-				Data Temp = Arr[MaxChild];
-				Arr[MaxChild] = Arr[CurIndex];
-				Arr[CurIndex] = Temp;
-			}
-
-			CurIndex = MaxChild;
-		}
-	}
-
-	void Reserve(const size_t& _Size)
-	{
-		Data* CopyArr = Arr;
-
-		if (_Size < Capacity)
-		{
-			return;
-		}
-
-		Arr = new Data[_Size]();
-
-		for (int i = 1; i <= Size; i++)
-		{
-			Arr[i] = CopyArr[i];
-		}
-
-		delete (CopyArr);
-		Capacity = _Size;
-	}
-
-	size_t Capacity = 0;
-	size_t Size = 0;
-
-	Data* Arr = nullptr;
-};
-
-int main()
-{
-	Priority_Queue<int> A;
-	A.Push(1);
-	A.Push(5);
-	A.Push(3);
-	A.Push(6);
-	A.Push(8);
-	A.Push(12);
-	A.Push(10);
-	A.Push(9);
-
-	int Arr[8];
-	for (int i = 0; i < 8; i++)
-	{
-		Arr[i] = A.Top();
-		A.Pop();
-	}
-
-	return 0;
-}
+//#include <queue>
+//#include <iostream>
+//
+//template <typename Data>
+//class Priority_Queue
+//{
+//public:
+//	Priority_Queue() 
+//	{
+//		Capacity = 4;
+//		Arr = new Data[Capacity]();
+//	}
+//
+//	~Priority_Queue() {}
+//
+//	void Push(const Data& _Data)
+//	{
+//		if (Size + 1 >= Capacity)
+//		{
+//			Reserve(Size * 2);
+//		}
+//
+//		Arr[Size + 1] = _Data;
+//		Heapify_Insert();
+//		
+//		Size++;
+//	}
+//
+//	Data Top()
+//	{
+//		return Arr[1];
+//	}
+//
+//	void Pop()
+//	{
+//		if (Size <= 0)
+//		{
+//			return;
+//		}
+//
+//		Arr[1] = Arr[Size];
+//		Size--;
+//
+//		Heapify_Delete();
+//	}
+//
+//private:
+//	void Heapify_Insert()
+//	{
+//		size_t NewDataIndex = Size + 1;
+//		size_t ParentIndex;
+//
+//		while (NewDataIndex > 1)
+//		{
+//			ParentIndex = NewDataIndex / 2;
+//
+//			if (Arr[ParentIndex] < Arr[NewDataIndex])
+//			{
+//				Data Temp = Arr[NewDataIndex];
+//				Data Copy = Arr[ParentIndex];
+//
+//				Arr[NewDataIndex] = Arr[ParentIndex];
+//				Arr[ParentIndex] = Temp;
+//
+//				NewDataIndex = ParentIndex;
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//	}
+//
+//	void Heapify_Delete()
+//	{
+//		size_t CurIndex = 1;
+//
+//		while (CurIndex <= (Size / 2))
+//		{
+//			size_t LeftChild = CurIndex * 2;
+//			size_t RightChild = CurIndex * 2 + 1;
+//
+//			size_t MaxChild = Arr[LeftChild] < Arr[RightChild] ? RightChild : LeftChild;
+//
+//			if (Arr[MaxChild] > Arr[CurIndex])
+//			{
+//				Data Temp = Arr[MaxChild];
+//				Arr[MaxChild] = Arr[CurIndex];
+//				Arr[CurIndex] = Temp;
+//			}
+//
+//			CurIndex = MaxChild;
+//		}
+//	}
+//
+//	void Reserve(const size_t& _Size)
+//	{
+//		Data* CopyArr = Arr;
+//
+//		if (_Size < Capacity)
+//		{
+//			return;
+//		}
+//
+//		Arr = new Data[_Size]();
+//
+//		for (int i = 1; i <= Size; i++)
+//		{
+//			Arr[i] = CopyArr[i];
+//		}
+//
+//		delete (CopyArr);
+//		Capacity = _Size;
+//	}
+//
+//	size_t Capacity = 0;
+//	size_t Size = 0;
+//
+//	Data* Arr = nullptr;
+//};
+//
+//int main()
+//{
+//	Priority_Queue<int> A;
+//	A.Push(1);
+//	A.Push(5);
+//	A.Push(3);
+//	A.Push(6);
+//	A.Push(8);
+//	A.Push(12);
+//	A.Push(10);
+//	A.Push(9);
+//
+//	int Arr[8];
+//	for (int i = 0; i < 8; i++)
+//	{
+//		Arr[i] = A.Top();
+//		A.Pop();
+//	}
+//
+//	return 0;
+//}
--- CodingTest/CodingTest/Map 구현.cpp
+++ CodingTest/CodingTest/Map 구현.cpp
@@ -1,493 +1,536 @@
-//#include <map>
-//#include <cassert>
-//#include <iostream>
-//
-////typedef int T;
-//
-//
-//template <typename KeyType, typename DataType>
-//class MyMap
-//{
-//
-//private:
-//	enum class NodeColor : char
-//	{
-//		Red,
-//		Black,
-//	};
-//
-//	struct Node
-//	{
-//		KeyType Key;
-//		DataType Data;
-//
-//		NodeColor Color = NodeColor::Black;
-//
-//		Node* Parent = nullptr;
-//		Node* LeftChild = NilNode;
-//		Node* RightChild = NilNode;
-//	};
-//
-//	static Node* NilNode;
-//
-//public:
-//	MyMap() 
-//	{
-//		CreateNilNode();
-//	}
-//
-//	~MyMap() {}
-//
-//	class Iterator
-//	{
-//	public:
-//		Iterator(Node* InitNode)
-//		{
-//			CurNode = InitNode;
-//		}
-//		
-//		Node* operator*() const
-//		{
-//			return CurNode;
-//		}
-//
-//		Node* operator->() const
-//		{
-//			return CurNode;
-//		}
-//
-//		void operator=(const DataType& _Data)
-//		{
-//			CurNode->Data = _Data;
-//		}
-//
-//		bool operator==(const Node* _Ptr) const
-//		{
-//			if (_Ptr == CurNode)
-//			{
-//				return true;
-//			}
-//			
-//			return false;
-//		}
-//
-//	private:
-//		Node* CurNode;
-//	};
-//
-//public:
-//
-//	DataType& operator[](const KeyType _Key) const
-//	{
-//		Iterator FindIter = Find(_Key);
-//		
-//		assert(FindIter != nullptr);
-//		std::cout << "맵에 유효하지 않은 인덱스로 접근하려 하였습니다. [존재하지 않는 키]";
-//
-//		return FindIter->Data;
-//	}
-//
-//	Iterator Find(const KeyType& _Key) const
-//	{
-//		Node* CurNode = RootNode;
-//
-//		while (CurNode != NilNode)
-//		{
-//			if (CurNode->Key == _Key)
-//			{
-//				break;
-//			}
-//			else if (CurNode->Key > _Key)
-//			{
-//				CurNode = CurNode->LeftChild;
-//			}
-//			else
-//			{
-//				CurNode = CurNode->RightChild;
-//			}
-//		}
-//
-//		if (CurNode == NilNode)
-//		{
-//			return Iterator(nullptr);
-//		}
-//		else
-//		{
-//			return Iterator(CurNode);
-//		}
-//	}
-//
-//	void insert(const KeyType& _Key, const DataType& _Data)
-//	{
-//		Node* NewNode = nullptr;
-//
-//		//사이즈가 0이면, 루트노드로 설정
-//		if (Size == 0)
-//		{
-//			NewNode = new Node();
-//			NewNode->Key = _Key;
-//			NewNode->Data = _Data;
-//
-//			RootNode = NewNode;
-//			RootNode->Color = NodeColor::Black;
-//
-//			Size++;
-//
-//			return;
-//		}
-//		
-//		else
-//		{
-//			Node* CurNode = RootNode;
-//			
-//			//삽입
-//			while (true)
-//			{
-//				//키가 같다면, 데이터만 바꾸고 끝.
-//				if (_Key == CurNode->Key)
-//				{
-//					CurNode->Data = _Data;
-//					return;
-//				}
-//				//데이터가 더 작다면, 해당 노드의 왼쪽노드를 탐색.
-//				else if (_Key < CurNode->Key)
-//				{
-//					//노드의 왼쪽 노드가 nullptr이면, 새로 들어온 노드를 왼쪽 노드로 설정.
-//					if (CurNode->LeftChild == NilNode)
-//					{
-//						NewNode = new Node();
-//
-//						NewNode->Key = _Key;
-//						NewNode->Data = _Data;
-//
-//						NewNode->Parent = CurNode;
-//						NewNode->Color = NodeColor::Red;
-//						
-//						CurNode->LeftChild = NewNode;
-//
-//						Size++;
-//
-//						break;
-//					}
-//					else
-//					{
-//						CurNode = CurNode->LeftChild;
-//						continue;
-//					}
-//				}
-//				else if (_Key > CurNode->Key)
-//				{
-//					//노드의 오른족 노드가 nullptr이면, 새로 들어온 노드를 오른쪽 노드로 설정.
-//					if (CurNode->RightChild == NilNode)
-//					{
-//						NewNode = new Node();
-//
-//						NewNode->Key = _Key;
-//						NewNode->Data = _Data;
-//
-//						NewNode->Parent = CurNode;
-//						NewNode->Color = NodeColor::Red;
-//
-//						CurNode->RightChild = NewNode;
-//
-//						Size++;
-//
-//						break;
-//					}
-//					else
-//					{
-//						CurNode = CurNode->RightChild;
-//						continue;
-//					}
-//				}
-//			}
-//
-//			//정렬
-//			if (NewNode->Parent == nullptr)
-//			{
-//				return;
-//			}
-//
-//			assert(NewNode->Parent != NilNode);
-//			ReBuilding(NewNode);
-//		}
-//	}
-//
-//	//Debug 용도, int에서만 쓰자.
-//	void PrintBySort(Node* _Node)
-//	{
-//		if (_Node == NilNode)
-//		{
-//			return;
-//		}
-//
-//		PrintBySort(_Node->LeftChild);
-//		std::cout << "Key : " << _Node->Key << " Data : " << _Node->Data << " Color : " << static_cast<int>(_Node->Color);
-//		std::cout << std::endl;
-//		PrintBySort(_Node->RightChild);
-//	}
-//
-//private:
-//	void LeftRotate(Node* _ParentNode)
-//	{
-//		Node* LeftChild = _ParentNode->LeftChild;
-//		Node* RightChild = _ParentNode->RightChild;
-//
-//		if (RightChild != NilNode) 
-//		{
-//			_ParentNode->RightChild = RightChild->LeftChild;
-//			
-//			if(RightChild->LeftChild != NilNode)
-//			{
-//				RightChild->LeftChild->Parent = _ParentNode;
-//			}
-//
-//			RightChild->Parent = _ParentNode->Parent;
-//
-//			if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->LeftChild)
-//			{
-//				_ParentNode->Parent->LeftChild = RightChild;
-//			}
-//			else if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->RightChild)
-//			{
-//				_ParentNode->Parent->RightChild = RightChild;
-//			}
-//
-//			_ParentNode->Parent = RightChild;
-//			RightChild->LeftChild = _ParentNode;
-//
-//			if (RightChild->Parent == nullptr)
-//			{
-//				RootNode = RightChild;
-//			}
-//		}
-//	}
-//
-//	void RightRotate(Node* _ParentNode)
-//	{
-//		Node* LeftChild = _ParentNode->LeftChild;
-//		Node* RightChild = _ParentNode->RightChild;
-//
-//		if (LeftChild != NilNode)
-//		{
-//			_ParentNode->LeftChild = LeftChild->RightChild;
-//
-//			if (LeftChild->RightChild != NilNode)
-//			{
-//				LeftChild->RightChild->Parent = _ParentNode;
-//			}
-//
-//			LeftChild->Parent = _ParentNode->Parent;
-//
-//			if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->LeftChild)
-//			{
-//				_ParentNode->Parent->LeftChild = LeftChild;
-//			}
-//			else if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->RightChild)
-//			{
-//				_ParentNode->Parent->RightChild = LeftChild;
-//			}
-//
-//			_ParentNode->Parent = LeftChild;
-//			LeftChild->RightChild = _ParentNode;
-//
-//			if (LeftChild->Parent == nullptr)
-//			{
-//				RootNode = LeftChild;
-//			}
-//		}
-//	}
-//
-//	void ReBuilding(Node* _CurNode)
-//	{
-//		Node* ParentNode = nullptr;
-//		Node* AncestorNode = nullptr;
-//		Node* UncleNode = nullptr;
-//
-//		Node* CurNode = _CurNode;
-//
-//		while(true)
-//		{
-//			if (CurNode == RootNode)
-//			{
-//				RootNode->Color = NodeColor::Black;
-//				break;
-//			}
-//
-//			if (CurNode->Parent == RootNode)
-//			{
-//				break;
-//			}
-//
-//			ParentNode = CurNode->Parent;
-//			AncestorNode = ParentNode->Parent;
-//
-//			if (!(CurNode->Color == NodeColor::Red && ParentNode->Color == NodeColor::Red))
-//			{
-//				break;
-//			}
-//
-//			if (ParentNode == AncestorNode->LeftChild)
-//			{
-//				UncleNode = AncestorNode->RightChild;
-//			}
-//			else if (ParentNode == AncestorNode->RightChild)
-//			{
-//				UncleNode = AncestorNode->LeftChild;
-//			}
-//			else
-//			{
-//				return;
-//			}
-//
-//			if (UncleNode->Color == NodeColor::Black)
-//			{
-//				ReStructing(CurNode, ParentNode, AncestorNode);
-//			}
-//			else if (UncleNode->Color == NodeColor::Red)
-//			{
-//				ReColoring(CurNode);
-//
-//				CurNode = AncestorNode;
-//			}
-//		}
-//	}
-//
-//	void ReStructing(Node* _CurNode, Node* _ParentNode, Node* _AncestorNode)
-//	{
-//		if (_ParentNode == nullptr)
-//		{
-//			return;
-//		}
-//
-//		if (_AncestorNode == nullptr)
-//		{
-//			return;
-//		}
-//
-//		if (_ParentNode == _AncestorNode->LeftChild)
-//		{
-//			if (_CurNode == _ParentNode->RightChild)
-//			{
-//				LeftRotate(_ParentNode);
-//
-//				//왼쪽회전 진행하면서, 부모노드와 현재노드의 관계가 바뀜.
-//				Node* Temp = _CurNode;
-//				_CurNode = _ParentNode;
-//				_ParentNode = Temp;
-//
-//			}
-//
-//			_ParentNode->Color = NodeColor::Black;
-//			_AncestorNode->Color = NodeColor::Red;
-//
-//			RightRotate(_AncestorNode);
-//		}
-//
-//		else if (_ParentNode == _AncestorNode->RightChild)
-//		{
-//			if (_CurNode == _ParentNode->LeftChild)
-//			{
-//				RightRotate(_ParentNode);
-//
-//				//왼쪽회전 진행하면서, 부모노드와 현재노드의 관계가 바뀜.
-//				Node* Temp = _CurNode;
-//				_CurNode = _ParentNode;
-//				_ParentNode = Temp;
-//
-//			}
-//
-//			_ParentNode->Color = NodeColor::Black;
-//			_AncestorNode->Color = NodeColor::Red;
-//
-//			LeftRotate(_AncestorNode);
-//		}
-//	}
-//
-//	void ReColoring(Node* _CurNode)
-//	{
-//		Node* ParentNode = _CurNode->Parent;
-//		Node* AncestorNode = ParentNode->Parent;
-//		Node* UncleNode = nullptr;
-//		//정상적인 상황이라면, 조상노드까지 nullptr일 수 없다.
-//		//루트 노드는 항상 검은색이라, 2~3층 이후부터만 이중 레드 충돌이 발생하기 때문.
-//		if (ParentNode == nullptr || AncestorNode == nullptr)
-//		{
-//			return;
-//		}
-//
-//		//루트노드가 들어오면 그냥 색만 바꾸고 종료.
-//		if (_CurNode == RootNode)
-//		{
-//			_CurNode->Color = NodeColor::Black;
-//			return;
-//		}
-//
-//		ParentNode->Color = NodeColor::Black;
-//		AncestorNode->Color = NodeColor::Red;
-//
-//		if (ParentNode == AncestorNode->LeftChild)
-//		{
-//			UncleNode = AncestorNode->RightChild;
-//		}
-//		else if (ParentNode == AncestorNode->RightChild)
-//		{
-//			UncleNode = AncestorNode->LeftChild;
-//		}
-//		else
-//		{
-//			return;
-//		}
-//
-//		if (UncleNode != NilNode)
-//		{
-//			UncleNode->Color = NodeColor::Black;
-//		}
-//
-//		_CurNode = AncestorNode;
-//	}
-//
-//	void CreateNilNode()
-//	{
-//		NilNode = new Node();
-//		NilNode->Color = NodeColor::Black;
-//	}
-//
-//	void SetBegin()
-//	{
-//		Node* CurNode = RootNode;
-//
-//		while (CurNode->LeftChild != nullptr)
-//		{
-//			CurNode = CurNode->LeftChild;
-//		}
-//		
-//		BeginNode = CurNode;
-//	}
-//
-//public:
-//	Node* RootNode = nullptr;
-//
-//private:
-//	Node* BeginNode = nullptr;
-//	size_t Size = 0;
-//};
-//
-//template <typename KeyType, typename DataType>
-//MyMap<KeyType, DataType>::Node* MyMap<KeyType, DataType>::NilNode = nullptr;
-//
-//int main()
-//{
-//
-//	MyMap<int, int> NewMap;
-//
-//
-//	for (int i = 0; i < 100; i++)
-//	{
-//		NewMap.insert(i, i);
-//	}
-//
-//	NewMap.PrintBySort(NewMap.RootNode);
-//
-//	return 0;
-//}
+#include <map>
+#include <cassert>
+#include <iostream>
+
+//typedef int T;
+
+
+template <typename KeyType, typename DataType>
+class MyMap
+{
+
+private:
+	enum class NodeColor : char
+	{
+		Red,
+		Black,
+	};
+
+	struct Node
+	{
+		KeyType Key;
+		DataType Data;
+
+		NodeColor Color = NodeColor::Black;
+
+		Node* Parent = nullptr;
+
+		Node* LeftChild = NilNode;
+		Node* RightChild = NilNode;
+	};
+
+	static Node* NilNode;
+
+public:
+	MyMap() 
+	{
+		CreateNilNode();
+	}
+
+	~MyMap() {}
+
+	class Iterator
+	{
+	public:
+		Iterator(Node* InitNode)
+		{
+			CurNode = InitNode;
+		}
+		
+		Node* operator*() const
+		{
+			return CurNode;
+		}
+
+		Node* operator->() const
+		{
+			return CurNode;
+		}
+
+		void operator=(const DataType& _Data)
+		{
+			CurNode->Data = _Data;
+		}
+
+		bool operator==(const Node* _Ptr) const
+		{
+			if (_Ptr == CurNode)
+			{
+				return true;
+			}
+			
+			return false;
+		}
+
+	private:
+		Node* CurNode;
+	};
+
+public:
+
+	DataType& operator[](const KeyType _Key) const
+	{
+		Iterator FindIter = Find(_Key);
+		
+		assert(FindIter != nullptr);
+		std::cout << "맵에 유효하지 않은 인덱스로 접근하려 하였습니다. [존재하지 않는 키]";
+
+		return FindIter->Data;
+	}
+
+	Iterator Find(const KeyType& _Key) const
+	{
+		Node* CurNode = RootNode;
+
+		while (CurNode != NilNode)
+		{
+			if (CurNode->Key == _Key)
+			{
+				break;
+			}
+			else if (CurNode->Key > _Key)
+			{
+				CurNode = CurNode->LeftChild;
+			}
+			else
+			{
+				CurNode = CurNode->RightChild;
+			}
+		}
+
+		if (CurNode == NilNode)
+		{
+			return Iterator(nullptr);
+		}
+		else
+		{
+			return Iterator(CurNode);
+		}
+	}
+
+	void insert(const KeyType& _Key, const DataType& _Data)
+	{
+		Node* NewNode = nullptr;
+
+		//사이즈가 0이면, 루트노드로 설정
+		if (Size == 0)
+		{
+			NewNode = new Node();
+			NewNode->Key = _Key;
+			NewNode->Data = _Data;
+
+			RootNode = NewNode;
+			RootNode->Color = NodeColor::Black;
+
+			Size++;
+
+			return;
+		}
+		
+		else
+		{
+			Node* CurNode = RootNode;
+			
+			//삽입
+			while (true)
+			{
+				//키가 같다면, 데이터만 바꾸고 끝.
+				if (_Key == CurNode->Key)
+				{
+					CurNode->Data = _Data;
+					return;
+				}
+				//데이터가 더 작다면, 해당 노드의 왼쪽노드를 탐색.
+				else if (_Key < CurNode->Key)
+				{
+					//노드의 왼쪽 노드가 nullptr이면, 새로 들어온 노드를 왼쪽 노드로 설정.
+					if (CurNode->LeftChild == NilNode)
+					{
+						NewNode = new Node();
+
+						NewNode->Key = _Key;
+						NewNode->Data = _Data;
+
+						NewNode->Parent = CurNode;
+						NewNode->Color = NodeColor::Red;
+						
+						CurNode->LeftChild = NewNode;
+
+						Size++;
+
+						break;
+					}
+					else
+					{
+						CurNode = CurNode->LeftChild;
+						continue;
+					}
+				}
+				else if (_Key > CurNode->Key)
+				{
+					//노드의 오른족 노드가 nullptr이면, 새로 들어온 노드를 오른쪽 노드로 설정.
+					if (CurNode->RightChild == NilNode)
+					{
+						NewNode = new Node();
+
+						NewNode->Key = _Key;
+						NewNode->Data = _Data;
+
+						NewNode->Parent = CurNode;
+						NewNode->Color = NodeColor::Red;
+
+						CurNode->RightChild = NewNode;
+
+						Size++;
+
+						break;
+					}
+					else
+					{
+						CurNode = CurNode->RightChild;
+						continue;
+					}
+				}
+			}
+
+			//정렬
+			if (NewNode->Parent == nullptr)
+			{
+				return;
+			}
+
+			assert(NewNode->Parent != NilNode);
+			ReBuilding(NewNode);
+		}
+	}
+
+	void Delete(const KeyType& _Key)
+	{
+		Iterator FindIter = Find(_Key);
+		Node* DeleteNode = *FindIter;
+
+		Node* SuccessorNode = nullptr;
+
+		Node* LeftChild = DeleteNode->LeftChild;
+		Node* RightChild = DeleteNode->RightChild;
+
+		//삭제할 노드의 자식이 없다면
+		if (LeftChild == NilNode && RightChild == NilNode)
+		{
+			//그냥 지운다. 부모노드의 자식에 닐노드를 대입.
+		}
+		//자식이 1개인데, 왼쪽 노드일 경우.
+		else if (LeftChild != NilNode && RightChild == NilNode)
+		{
+			//왼쪽 노드가 후임자가 된다.
+		}
+		//자식이 1개인데, 오른쪽 노드일 경우.
+		else if (LeftChild == NilNode && RightChild != NilNode)
+		{
+			//오른쪽 노드가 후임자가 된다.
+		}
+		//자식이 2개인 경우.
+		else if (LeftChild != NilNode && RightChild != NilNod)
+		{
+			//삭제하려는 노드가 루트노드의 왼쪽 서브트리에 있다면
+			//삭제하려는 노드의 오른쪽 서브트리에서 최소값을 가지는 노드가 후임자가 된다.
+
+			//삭제하려는 노드가 루트노드의 오른쪽 서브트리에 있다면
+			//삭제하려는 노드의 왼쪽 서브트리에서 최대값을 가지는 노드가 후임자가 된다.
+		}
+
+		/*
+		* if_1) 후임자 노드의 색이 Red 라면, 어떠한 속성도 위반하지 않는다.
+		* if_2) 후임자 노드의 색이 Black 이라면, Double-Black 발생 -> 해결해야함.
+		*/
+
+	}
+
+	//Debug 용도, int에서만 쓰자.
+	void PrintBySort(Node* _Node)
+	{
+		if (_Node == NilNode)
+		{
+			return;
+		}
+
+		PrintBySort(_Node->LeftChild);
+		std::cout << "Key : " << _Node->Key << " Data : " << _Node->Data << " Color : " << static_cast<int>(_Node->Color);
+		std::cout << std::endl;
+		PrintBySort(_Node->RightChild);
+	}
+
+private:
+	void LeftRotate(Node* _ParentNode)
+	{
+		Node* LeftChild = _ParentNode->LeftChild;
+		Node* RightChild = _ParentNode->RightChild;
+
+		if (RightChild != NilNode) 
+		{
+			_ParentNode->RightChild = RightChild->LeftChild;
+			
+			if(RightChild->LeftChild != NilNode)
+			{
+				RightChild->LeftChild->Parent = _ParentNode;
+			}
+
+			RightChild->Parent = _ParentNode->Parent;
+
+			if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->LeftChild)
+			{
+				_ParentNode->Parent->LeftChild = RightChild;
+			}
+			else if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->RightChild)
+			{
+				_ParentNode->Parent->RightChild = RightChild;
+			}
+
+			_ParentNode->Parent = RightChild;
+			RightChild->LeftChild = _ParentNode;
+
+			if (RightChild->Parent == nullptr)
+			{
+				RootNode = RightChild;
+			}
+		}
+	}
+
+	void RightRotate(Node* _ParentNode)
+	{
+		Node* LeftChild = _ParentNode->LeftChild;
+		Node* RightChild = _ParentNode->RightChild;
+
+		if (LeftChild != NilNode)
+		{
+			_ParentNode->LeftChild = LeftChild->RightChild;
+
+			if (LeftChild->RightChild != NilNode)
+			{
+				LeftChild->RightChild->Parent = _ParentNode;
+			}
+
+			LeftChild->Parent = _ParentNode->Parent;
+
+			if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->LeftChild)
+			{
+				_ParentNode->Parent->LeftChild = LeftChild;
+			}
+			else if (_ParentNode->Parent != nullptr && _ParentNode == _ParentNode->Parent->RightChild)
+			{
+				_ParentNode->Parent->RightChild = LeftChild;
+			}
+
+			_ParentNode->Parent = LeftChild;
+			LeftChild->RightChild = _ParentNode;
+
+			if (LeftChild->Parent == nullptr)
+			{
+				RootNode = LeftChild;
+			}
+		}
+	}
+
+	void ReBuilding(Node* _CurNode)
+	{
+		Node* ParentNode = nullptr;
+		Node* AncestorNode = nullptr;
+		Node* UncleNode = nullptr;
+
+		Node* CurNode = _CurNode;
+
+		while(true)
+		{
+			if (CurNode == RootNode)
+			{
+				RootNode->Color = NodeColor::Black;
+				break;
+			}
+
+			if (CurNode->Parent == RootNode)
+			{
+				break;
+			}
+
+			ParentNode = CurNode->Parent;
+			AncestorNode = ParentNode->Parent;
+
+			if (!(CurNode->Color == NodeColor::Red && ParentNode->Color == NodeColor::Red))
+			{
+				break;
+			}
+
+			if (ParentNode == AncestorNode->LeftChild)
+			{
+				UncleNode = AncestorNode->RightChild;
+			}
+			else if (ParentNode == AncestorNode->RightChild)
+			{
+				UncleNode = AncestorNode->LeftChild;
+			}
+			else
+			{
+				return;
+			}
+
+			if (UncleNode->Color == NodeColor::Black)
+			{
+				ReStructing(CurNode, ParentNode, AncestorNode);
+			}
+			else if (UncleNode->Color == NodeColor::Red)
+			{
+				ReColoring(CurNode);
+
+				CurNode = AncestorNode;
+			}
+		}
+	}
+
+	void ReStructing(Node* _CurNode, Node* _ParentNode, Node* _AncestorNode)
+	{
+		if (_ParentNode == nullptr)
+		{
+			return;
+		}
+
+		if (_AncestorNode == nullptr)
+		{
+			return;
+		}
+
+		if (_ParentNode == _AncestorNode->LeftChild)
+		{
+			if (_CurNode == _ParentNode->RightChild)
+			{
+				LeftRotate(_ParentNode);
+
+				//왼쪽회전 진행하면서, 부모노드와 현재노드의 관계가 바뀜.
+				Node* Temp = _CurNode;
+				_CurNode = _ParentNode;
+				_ParentNode = Temp;
+
+			}
+
+			_ParentNode->Color = NodeColor::Black;
+			_AncestorNode->Color = NodeColor::Red;
+
+			RightRotate(_AncestorNode);
+		}
+
+		else if (_ParentNode == _AncestorNode->RightChild)
+		{
+			if (_CurNode == _ParentNode->LeftChild)
+			{
+				RightRotate(_ParentNode);
+
+				//왼쪽회전 진행하면서, 부모노드와 현재노드의 관계가 바뀜.
+				Node* Temp = _CurNode;
+				_CurNode = _ParentNode;
+				_ParentNode = Temp;
+
+			}
+
+			_ParentNode->Color = NodeColor::Black;
+			_AncestorNode->Color = NodeColor::Red;
+
+			LeftRotate(_AncestorNode);
+		}
+	}
+
+	void ReColoring(Node* _CurNode)
+	{
+		Node* ParentNode = _CurNode->Parent;
+		Node* AncestorNode = ParentNode->Parent;
+		Node* UncleNode = nullptr;
+		//정상적인 상황이라면, 조상노드까지 nullptr일 수 없다.
+		//루트 노드는 항상 검은색이라, 2~3층 이후부터만 이중 레드 충돌이 발생하기 때문.
+		if (ParentNode == nullptr || AncestorNode == nullptr)
+		{
+			return;
+		}
+
+		//루트노드가 들어오면 그냥 색만 바꾸고 종료.
+		if (_CurNode == RootNode)
+		{
+			_CurNode->Color = NodeColor::Black;
+			return;
+		}
+
+		ParentNode->Color = NodeColor::Black;
+		AncestorNode->Color = NodeColor::Red;
+
+		if (ParentNode == AncestorNode->LeftChild)
+		{
+			UncleNode = AncestorNode->RightChild;
+		}
+		else if (ParentNode == AncestorNode->RightChild)
+		{
+			UncleNode = AncestorNode->LeftChild;
+		}
+		else
+		{
+			return;
+		}
+
+		if (UncleNode != NilNode)
+		{
+			UncleNode->Color = NodeColor::Black;
+		}
+
+		_CurNode = AncestorNode;
+	}
+
+	void CreateNilNode()
+	{
+		NilNode = new Node();
+		NilNode->Color = NodeColor::Black;
+	}
+
+	void SetBegin()
+	{
+		Node* CurNode = RootNode;
+
+		while (CurNode->LeftChild != nullptr)
+		{
+			CurNode = CurNode->LeftChild;
+		}
+		
+		BeginNode = CurNode;
+	}
+
+public:
+	Node* RootNode = nullptr;
+
+private:
+	Node* BeginNode = nullptr;
+	size_t Size = 0;
+};
+
+template <typename KeyType, typename DataType>
+MyMap<KeyType, DataType>::Node* MyMap<KeyType, DataType>::NilNode = nullptr;
+
+int main()
+{
+
+	MyMap<int, int> NewMap;
+
+
+	for (int i = 0; i < 100; i++)
+	{
+		NewMap.insert(i, i);
+	}
+	NewMap.Delete(50);
+	NewMap.PrintBySort(NewMap.RootNode);
+
+	return 0;
+}
