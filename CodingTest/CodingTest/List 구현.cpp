06a9cac488eaffd0a16a44f24c692f5b4084f1f9
EuihyeonO <yuu5666@naver.com>
2024-03-30 19:10:32 +0900

    List 구현 업데이트

--- CodingTest/CodingTest/List 구현.cpp
+++ CodingTest/CodingTest/List 구현.cpp
@@ -1,208 +1,213 @@
-//#include <list>
-//#include <memory>
-//#include <vector>
-//
-//
-//template <typename T>
-//class List 
-//{
-//private:
-//
-//	struct Data
-//	{
-//		T* OwnData;
-//
-//		Data* PrevData;
-//		Data* NextData;
-//	};
-//
-//public:
-//
-//	struct Iterator
-//	{
-//		Data* Data;
-//
-//		Iterator operator++(int _Num)
-//		{
-//			if (Data == nullptr)
-//			{
-//				return Iterator(nullptr);
-//			}
-//
-//			Iterator NewIter = { Data };
-//			Data = Data->NextData;
-//
-//			return NewIter;
-//		}
-//
-//		T& operator*()
-//		{
-//			return *(Data->OwnData);
-//		}
-//	};
-//
-////생성자
-//public:
-//	List(){}
-//
-//	~List()
-//	{
-//		Clear();
-//	}
-//
-//	List(size_t _Size)
-//	{
-//		if (Begin == nullptr)
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T();
-//			NewData->PrevData = nullptr;
-//			NewData->NextData = nullptr;
-//
-//			Begin = NewData;
-//			End = NewData;
-//		}
-//
-//		for (int i = 0; i < _Size - 1; i++)
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T();
-//
-//			NewData->PrevData = End;
-//			End->NextData = NewData;
-//
-//			NewData->NextData = nullptr;
-//
-//			End = NewData;
-//		}
-//	}
-//
-//	List(size_t _Size, const T& _Data)
-//	{
-//		if (Begin == nullptr)
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T(_Data);
-//			NewData->PrevData = nullptr;
-//			NewData->NextData = nullptr;
-//
-//			Begin = NewData;
-//			End = NewData;
-//
-//			++Size;
-//		}
-//
-//		for (int i = 0; i < _Size - 1; i++)
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T(_Data);
-//
-//			NewData->PrevData = End;
-//			End->NextData = NewData;
-//
-//			NewData->NextData = nullptr;
-//
-//			End = NewData;
-//
-//			++Size;
-//		}
-//	}
-////기능
-//public:
-//	void push_back(const T& _Data)
-//	{
-//
-//		if (Begin == nullptr)
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T(_Data);
-//			NewData->PrevData = nullptr;
-//			NewData->NextData = nullptr;
-//
-//			Begin = NewData;
-//			End = NewData;
-//		}
-//		else
-//		{
-//			Data* NewData = new Data();
-//			NewData->OwnData = new T(_Data);
-//
-//			NewData->PrevData = End;
-//			End->NextData = NewData;
-//
-//			NewData->NextData = nullptr;
-//
-//			End = NewData;
-//		}
-//
-//		++Size;
-//	}
-//
-//	void Clear()
-//	{
-//		while (Begin != nullptr)
-//		{
-//			delete (Begin->OwnData);
-//
-//			Data* PrevData = Begin;
-//			Begin = Begin->NextData;
-//			
-//			delete (PrevData);
-//
-//			Size--;
-//		}
-//
-//		Begin = nullptr;
-//		End = nullptr;
-//
-//		Size = 0;
-//	}
-//
-//	Iterator begin()
-//	{
-//		Iterator NewIter = { Begin };
-//		return NewIter;
-//	}
-//
-//private:
-//	Data* Begin = nullptr;
-//	Data* End = nullptr;
-//
-//	size_t Size = 0;
-//};
-//
-//class A
-//{
-//public:
-//	A(){}
-//	~A(){}
-//	
-//	A(A&& _Ref) noexcept
-//	{
-//		int a = 0;
-//	}
-//
-//	A(const A& _Ref)
-//	{
-//		int a = 0;
-//	}
-//};
-//
-//int main()
-//{
-//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-// 
-//	List<int> MyList;
-//
-//	MyList.push_back(0);
-//	MyList.push_back(1);
-//	MyList.push_back(2);
-//	MyList.push_back(3);
-//
-//	MyList.Clear();
-//
-//	MyList.push_back(1);
-//	MyList.push_back(1);
-//	MyList.push_back(1);
-//	MyList.push_back(1);
-//}
+#include <list>
+#include <iostream>
+
+class Iterator;
+template <typename DataType>
+class List
+{
+public:
+	List()
+	{
+		size_t DataSize = 0;
+
+		Node* Head = nullptr;
+		Node* Tail = nullptr;
+	}
+
+	List(size_t _Size)
+	{
+		Resize(_Size);
+	}
+
+	List(size_t _Size, const DataType& _Data)
+	{
+		Resize(_Data);
+	}
+
+	~List()
+	{
+		for (size_t i = DataSize; i > 0; i--)
+		{
+			Pop_Back();
+		}
+	}
+
+public:
+	void Push_Back(const DataType& _Data)
+	{
+		Node* NewNode = new Node();
+		NewNode->Data = _Data;
+
+		if (DataSize == 0)
+		{
+			Head = NewNode;
+			Tail = NewNode;
+		}
+		else
+		{
+			NewNode->Prev = Tail;
+			Tail->Next = NewNode;
+			Tail = NewNode;
+		}
+
+		DataSize++;
+	}
+	
+	void Pop_Back()
+	{
+		if (DataSize == 0)
+		{
+			return;
+		}
+
+		if (DataSize > 1)
+		{
+			Node* TailPrev = Tail->Prev;
+			TailPrev->Next = nullptr;
+
+			delete Tail;
+			Tail = TailPrev;
+		}
+		else
+		{
+			delete Head;
+
+			Head = nullptr;
+			Tail = nullptr;
+		}
+		
+		DataSize--;
+	}
+
+	void Resize(size_t _Size)
+	{
+		if (_Size >= DataSize)
+		{
+			for (size_t i = DataSize; i < _Size; i++)
+			{
+				Push_Back(DataType());
+			}
+		}
+		else
+		{
+			for (size_t i = DataSize; i > _Size; i--)
+			{
+				Pop_Back();
+			}
+		}
+
+		DataSize = _Size;
+	}
+
+	void Resize(size_t _Size, const DataType& _Data)
+	{
+		if (_Size >= DataSize)
+		{
+			for (size_t i = DataSize; i < _Size; i++)
+			{
+				Push_Back(_Data);
+			}
+		}
+		else
+		{
+			for (size_t i = DataSize; i > _Size; i--)
+			{
+				Pop_Back();
+			}
+		}
+
+		DataSize = _Size;
+	}
+
+//private는 맨 밑으로 보내고 싶은데, 선언부 구현부 분리 안하니까 이런 일이 생긴다!
+private:
+	struct Node
+	{
+		friend Iterator;
+
+		DataType Data = DataType();
+
+		Node* Prev = nullptr;
+		Node* Next = nullptr;
+	};
+
+public:
+	class Iterator
+	{
+	public:
+		Iterator(const Node* _Node)
+		{
+			CurNode = _Node;
+		}
+
+		Iterator(const Iterator& _Iter)
+		{
+			CurNode = _Iter->CurNode;
+		}
+
+		DataType& operator*()
+		{
+			return CurNode->Data;
+		}
+
+		Iterator& operator++()
+		{
+			CurNode = CurNode->Next;
+			return this;
+		}
+
+		Iterator operator++(int)
+		{
+			Iterator ReturnIter;
+			ReturnIter->CurNode = CurNode;
+
+			CurNode = CurNode->Next;
+			return ReturnIter;
+		}
+
+		Iterator& operator--()
+		{
+			CurNode = CurNode->Prev;
+			return this;
+		}
+
+		Iterator operator--(int)
+		{
+			Iterator ReturnIter;
+			ReturnIter->CurNode = CurNode;
+
+			CurNode = CurNode->Prev;
+			return ReturnIter;
+		}
+
+	private:
+		Node* CurNode = nullptr;
+	};
+
+private:
+	size_t DataSize = 0;
+
+	Node* Head = nullptr;
+	Node* Tail = nullptr;
+};
+
+#include <memory>
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	List<int> A(10);
+	
+	List<int> B;
+	for (int i = 0; i < 10; i++)
+	{
+		B.Push_Back(i);
+	}
+
+	List<int> C(10);
+	for (int i = 0; i < 10; i++)
+	{
+		B.Pop_Back();
+	}
+}
--- CodingTest/CodingTest/Vector 구현.cpp
+++ CodingTest/CodingTest/Vector 구현.cpp
@@ -1,255 +1,253 @@
-
-#include <assert.h>
-#include <memory>
-#include <iostream>
-#include <vector>
-#include <chrono>
-
-template <typename DataType>
-class Vector
-{
-public:
-
-    class Iterator
-    {
-    public:
-        Iterator()
-        {
-            CurElement = nullptr;
-        }
-
-        Iterator(DataType* _Data)
-        {
-            CurElement = _Data;
-        }
-
-        Iterator(const Iterator& _Iter)
-        {
-            CurElement = _Iter.CurElement;
-        }
-
-        Iterator& operator++()
-        {
-            ++CurElement;
-            return *this;
-        }
-
-        Iterator operator++(int)
-        {
-            Iterator Iter = *this;
-            ++CurElement;
-
-            return Iter;
-        }
-
-        DataType& operator*()
-        {
-            return *CurElement;
-        }
-
-        size_t operator-(const Iterator& other) const
-        {
-            return CurElement - other.CurElement;
-        }
-
-    private:
-        DataType* CurElement = nullptr;
-    };
-
-public:
-    Vector()
-    {
-        Reserve(4);
-    }
-
-    Vector(size_t _Size)
-    {
-        Resize(_Size);
-    }
-
-    Vector(size_t _Size, const DataType& _Data)
-    {
-        Resize(_Size, _Data);
-    }
-
-    ~Vector()
-    {
-        delete[] Array;
-    }
-
-    DataType operator[](size_t _Index)
-    {
-        assert(_Index < DataSize);
-
-        return Array[_Index];
-    }
-
-public:
-
-    Iterator Begin()
-    {
-        Iterator BeginIter(Array);
-        return BeginIter;
-    }
-
-    Iterator End()
-    {
-        Iterator BeginIter(Array + DataSize);
-        return BeginIter;
-    }
-
-    void Reserve(size_t _Size)
-    {
-        if (_Size <= DataCapacity)
-        {
-            return;
-        }
-
-        DataCapacity = _Size;
-
-        if (Array == nullptr)
-        {
-            Array = new DataType[DataCapacity]();
-        }
-        else
-        {
-            DataType* NewArray = new DataType[_Size]();         
-            for (size_t i = 0; i < DataSize; i++)
-            {
-                NewArray[i] = Array[i];
-            }
-
-            delete[] Array;
-            Array = NewArray;
-        }
-    }
-
-    void Resize(size_t _Size)
-    {
-        if (DataSize >= _Size)
-        {
-            DataSize = _Size;
-        }
-        else 
-        {
-            Reserve(_Size);
-            DataSize = _Size;
-        }
-    }
-
-    void Resize(size_t _Size, const DataType& _Data)
-    {
-        if (DataSize >= _Size)
-        {
-            DataSize = _Size;
-        }
-        else
-        {
-            Reserve(_Size);
-    
-            for (size_t i = DataSize; i < _Size; i++)
-            {
-                Array[i] = _Data;
-            }
-
-            DataSize = _Size;
-        }
-    }
-
-    void Push_Back(const DataType& _Data)
-    {
-        if (DataSize < DataCapacity)
-        {
-            Array[DataSize] = _Data;
-        }
-        else
-        {
-            Reserve(DataCapacity * 2);
-            Array[DataSize] = _Data;
-        }
-
-        DataSize++;
-    }
-
-    void Pop_back()
-    {
-        if (DataSize > 0)
-        {
-            DataSize--;
-        }
-    }
-
-    Iterator Erase(const Iterator& _Iter)
-    {
-        size_t Index = _Iter - Begin();
-
-        for (size_t i = Index; i < DataSize - 1; i++)
-        {
-            Array[i] = Array[i + 1];
-        }
-
-        DataSize--;
-
-        return Iterator(&Array[Index]);
-    }
-    
-    void Insert(const Iterator& _Iter, const DataType& _Data)
-    {
-        if (DataSize >= DataCapacity)
-        {
-            Reserve(DataCapacity * 2);
-        }
-
-        size_t Index = _Iter - Begin();
-        
-        for (size_t i = DataSize; i > Index; i--)
-        {
-            Array[i] = Array[i - 1];
-        }
-        
-        Array[Index] = _Data;
-
-        DataSize++;
-    }
-
-    size_t Size()
-    {
-        return DataSize;
-    }
-
-    size_t Capacity()
-    {
-        return DataCapacity;
-    }
-
-private:
-    DataType* Array = nullptr;
-
-    size_t DataCapacity = 0;
-    size_t DataSize = 0;
-};
-
-int main()
-{
-    std::vector<int> STL;
-    Vector<int> MyVec;
-
-    std::chrono::system_clock::time_point start1 = std::chrono::system_clock::now();
-   
-    for (int i = 0; i < 10000000; i++)
-    {
-        MyVec.Push_Back(i);
-    }
-
-    std::chrono::milliseconds MyResult = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start1);
-
-    std::chrono::system_clock::time_point start2 = std::chrono::system_clock::now();
-    for (int i = 0; i < 10000000; i++)
-    {
-        STL.push_back(i);
-    }
-
-    std::chrono::milliseconds STLResult = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start2);
-
-    int a = 0;
-}
+//
+//#include <assert.h>
+//#include <chrono>
+//
+//template <typename DataType>
+//class Vector
+//{
+//public:
+//
+//    class Iterator
+//    {
+//    public:
+//        Iterator()
+//        {
+//            CurElement = nullptr;
+//        }
+//
+//        Iterator(DataType* _Data)
+//        {
+//            CurElement = _Data;
+//        }
+//
+//        Iterator(const Iterator& _Iter)
+//        {
+//            CurElement = _Iter.CurElement;
+//        }
+//
+//        Iterator& operator++()
+//        {
+//            ++CurElement;
+//            return *this;
+//        }
+//
+//        Iterator operator++(int)
+//        {
+//            Iterator Iter = *this;
+//            ++CurElement;
+//
+//            return Iter;
+//        }
+//
+//        DataType& operator*()
+//        {
+//            return *CurElement;
+//        }
+//
+//        size_t operator-(const Iterator& other) const
+//        {
+//            return CurElement - other.CurElement;
+//        }
+//
+//    private:
+//        DataType* CurElement = nullptr;
+//    };
+//
+//public:
+//    Vector()
+//    {
+//        Reserve(4);
+//    }
+//
+//    Vector(size_t _Size)
+//    {
+//        Resize(_Size);
+//    }
+//
+//    Vector(size_t _Size, const DataType& _Data)
+//    {
+//        Resize(_Size, _Data);
+//    }
+//
+//    ~Vector()
+//    {
+//        delete[] Array;
+//    }
+//
+//    DataType operator[](size_t _Index)
+//    {
+//        assert(_Index < DataSize);
+//
+//        return Array[_Index];
+//    }
+//
+//public:
+//
+//    Iterator Begin()
+//    {
+//        Iterator BeginIter(Array);
+//        return BeginIter;
+//    }
+//
+//    Iterator End()
+//    {
+//        Iterator BeginIter(Array + DataSize);
+//        return BeginIter;
+//    }
+//
+//    void Reserve(size_t _Size)
+//    {
+//        if (_Size <= DataCapacity)
+//        {
+//            return;
+//        }
+//
+//        DataCapacity = _Size;
+//
+//        if (Array == nullptr)
+//        {
+//            Array = new DataType[DataCapacity]();
+//        }
+//        else
+//        {
+//            DataType* NewArray = new DataType[_Size]();         
+//            for (size_t i = 0; i < DataSize; i++)
+//            {
+//                NewArray[i] = Array[i];
+//            }
+//
+//            delete[] Array;
+//            Array = NewArray;
+//        }
+//    }
+//
+//    void Resize(size_t _Size)
+//    {
+//        if (DataSize >= _Size)
+//        {
+//            DataSize = _Size;
+//        }
+//        else 
+//        {
+//            Reserve(_Size);
+//            DataSize = _Size;
+//        }
+//    }
+//
+//    void Resize(size_t _Size, const DataType& _Data)
+//    {
+//        if (DataSize >= _Size)
+//        {
+//            DataSize = _Size;
+//        }
+//        else
+//        {
+//            Reserve(_Size);
+//    
+//            for (size_t i = DataSize; i < _Size; i++)
+//            {
+//                Array[i] = _Data;
+//            }
+//
+//            DataSize = _Size;
+//        }
+//    }
+//
+//    void Push_Back(const DataType& _Data)
+//    {
+//        if (DataSize < DataCapacity)
+//        {
+//            Array[DataSize] = _Data;
+//        }
+//        else
+//        {
+//            Reserve(DataCapacity * 2);
+//            Array[DataSize] = _Data;
+//        }
+//
+//        DataSize++;
+//    }
+//
+//    void Pop_back()
+//    {
+//        if (DataSize > 0)
+//        {
+//            DataSize--;
+//        }
+//    }
+//
+//    Iterator Erase(const Iterator& _Iter)
+//    {
+//        size_t Index = _Iter - Begin();
+//
+//        for (size_t i = Index; i < DataSize - 1; i++)
+//        {
+//            Array[i] = Array[i + 1];
+//        }
+//
+//        DataSize--;
+//
+//        return Iterator(&Array[Index]);
+//    }
+//    
+//    void Insert(const Iterator& _Iter, const DataType& _Data)
+//    {
+//        if (DataSize >= DataCapacity)
+//        {
+//            Reserve(DataCapacity * 2);
+//        }
+//
+//        size_t Index = _Iter - Begin();
+//        
+//        for (size_t i = DataSize; i > Index; i--)
+//        {
+//            Array[i] = Array[i - 1];
+//        }
+//        
+//        Array[Index] = _Data;
+//
+//        DataSize++;
+//    }
+//
+//    size_t Size()
+//    {
+//        return DataSize;
+//    }
+//
+//    size_t Capacity()
+//    {
+//        return DataCapacity;
+//    }
+//
+//private:
+//    DataType* Array = nullptr;
+//
+//    size_t DataCapacity = 0;
+//    size_t DataSize = 0;
+//};
+//
+//
+//int main()
+//{
+//    std::vector<int> STL;
+//    Vector<int> MyVec;
+//
+//    std::chrono::system_clock::time_point start1 = std::chrono::system_clock::now();
+//   
+//    for (int i = 0; i < 10000000; i++)
+//    {
+//        MyVec.Push_Back(i);
+//    }
+//
+//    std::chrono::milliseconds MyResult = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start1);
+//
+//    std::chrono::system_clock::time_point start2 = std::chrono::system_clock::now();
+//    for (int i = 0; i < 10000000; i++)
+//    {
+//        STL.push_back(i);
+//    }
+//
+//    std::chrono::milliseconds STLResult = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start2);
+//
+//    int a = 0;
+//}
