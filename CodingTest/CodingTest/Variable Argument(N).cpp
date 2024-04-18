58b6ded09a024846eae88189c70318badb984389
EuihyeonO <yuu5666@naver.com>
2024-04-18 20:38:31 +0900

    가변 인자 템플릿 업데이트

--- CodingTest/CodingTest/Variable Argument(N).cpp
+++ CodingTest/CodingTest/Variable Argument(N).cpp
@@ -1,20 +1,74 @@
 //#include <string>
 //#include <iostream>
 //
-//template<typename T>
-//void Function1(T _Arg)
+////template<typename T>
+////void Print_1(T _FixArg)
+////{
+////    std::cout << _FixArg << " ";
+////}
+////
+////template<typename T1, typename... Types>
+////void Print_1(T1 _FixArg , Types... _VarArgs)
+////{
+////    std::cout << _FixArg << " ";
+////    Print_1(_VarArgs...);
+////}
+////
+////template<typename T>
+////int Sum(T _FixArg)
+////{
+////    return _FixArg;
+////}
+////
+////template<typename T1, typename... Types>
+////int Sum(T1 _FixArg, Types... _VarArgs)
+////{
+////    int Result = _FixArg + Sum(_VarArgs...);
+////    return Result;
+////}
+//
+///*-------------------------------------------------------------------------------------------*/
+//
+//template<typename ...Types>
+//int Sum(Types... _VarArgs)
 //{
-//    std::cout << _Arg;
+//    return (_VarArgs + ...);
 //}
 //
-//template<typename... Var>
-//void AddData(Var... Arg)
+//
+//template<typename ...Types>
+//int Sub_1(Types... _VarArgs)
 //{
-//    Function(Arg...);
+//    return (_VarArgs - ...);
+//}
+//
+//template<typename ...Types>
+//int Sub_2(Types... _VarArgs)
+//{
+//    return (... - _VarArgs);
+//}
+//
+//template<typename ...Types>
+//int Sub_3(Types... _VarArgs)
+//{
+//    return (1 - ... - _VarArgs);
+//}
+//
+//template<typename ...Types>
+//int Sub_4(Types... _VarArgs)
+//{
+//    return (_VarArgs - ... - 1);
+//}
+//
+//template<typename ...Types>
+//void PrintAll(Types... _VarArgs)
+//{
+//    (std::cout << ... << _VarArgs);
 //}
 //
 //int main()
 //{
-//     
+//    PrintAll(1, 2, 3, 4);
+//
 //    return 0;
 //}
