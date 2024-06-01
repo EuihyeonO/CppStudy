21b3dbb24bcf7ef6ae477ecd7162a389e6ac1a3f
EuihyeonO <yuu5666@naver.com>
2024-06-01 19:16:08 +0900

    파일 정리

---
+++ CodingTest/CodingTest/버튼 게임.cpp
@@ -0,0 +1,35 @@
+﻿//#include <iostream>
+//#include <climits>
+//
+//int GetGCD(int _A, int _B)
+//{
+//    while (_B > 0)
+//    {
+//        int Remain = _A % _B;
+//        _A = _B;
+//        _B = Remain;
+//    }
+//
+//    return _A;
+//}
+//
+//int main()
+//{
+//    int ATarget = 0;
+//    int BTarget = 0;
+//
+//    std::cin >> ATarget >> BTarget;
+//
+//    int GCD = GetGCD(ATarget, BTarget);
+//
+//    if (ATarget > 0 && GCD == 1)
+//    {
+//        std::cout << "POSSIBLE";
+//    }
+//    else
+//    {
+//        std::cout << "IMPOSSIBLE";
+//    }
+//
+//    return 0;
+//}
